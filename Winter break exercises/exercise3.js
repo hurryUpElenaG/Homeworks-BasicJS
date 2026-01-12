32c30050ab45b2ace66f650e709b46412bd530fa
ElenaGeorgievska <elena_science@yahoo.com>
2026-01-12 22:54:13 +0100

    fixed exercise3.js from winter break exercises

--- Winter break exercises/exercise3.js
+++ Winter break exercises/exercise3.js
@@ -2,9 +2,12 @@ function findCloserNumberTo100(number1, number2) {
   if (isNaN(number1) || number1 < 0 || isNaN(number2) || number2 < 0) {
     alert("Invalid number! Please enter a non-negative integer.");
   } else {
-    if (number1 > number2) {
+    let difference1 = Math.abs(100 - number1);
+    let difference2 = Math.abs(100 - number2);
+
+    if (difference1 < difference2) {
       alert(`${number1} is closer to 100!`);
-    } else if (number2 > number1) {
+    } else if (difference2 < difference1) {
       alert(`${number2} is closer to 100!`);
     } else {
       alert("Both numbers are equally close to 100");
@@ -12,7 +15,7 @@ function findCloserNumberTo100(number1, number2) {
   }
 }
 
-findCloserNumberTo100(20, 80);
+findCloserNumberTo100(-20, 80);
 
 /*let input1 = prompt("Enter first number:");
 let number1 = parseInt(input1);
@@ -21,5 +24,3 @@ let input2 = prompt("Enter second number:");
 let number2 = parseInt(input2);
 
 findCloserNumberTo100(number1, number2); */
-
-
